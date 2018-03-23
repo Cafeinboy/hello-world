9690aef88093dceeec9df61aa663d069cad45866
HIVET Kévin <kevin.hivet@student.ecp.fr>
2018-03-23 19:22:30 +0100

    First MAJ

---
+++ hello-world/src/PrintHelloWorld/LOL.java
@@ -0,0 +1,11 @@
+package PrintHelloWorld;
+
+public class LOL {
+	
+	public static void main(String[] args) {
+		System.out.println("LOL");
+		System.out.println("J'ai oublié un truc");
+		System.out.println("Hello world");
+	}
+
+}
